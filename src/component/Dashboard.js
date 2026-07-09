e0bd594385d049e91de734674b16b6d191180cc1
akintanseyi <seyiakintan@yahoo.com>
2026-07-09 15:33:35 -0700

    Initial clean commit

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -247,12 +247,12 @@ const EnergyLandingPage = () => {
                 </p>
               </div>
 
-              <div className="flex items-center gap-4 text-slate-400 group">
+              {/* <div className="flex items-center gap-4 text-slate-400 group">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                energydao@energy.gov
-              </div>
+              </div> */}
             </div>
 
             {/* RIGHT SIDE: LOGO & TAGLINE */}
